d48d9202827075d609efab895ea6a10d1b07f0ea
Christoph Stephan <chm.stephan@outlook.com>
2018-08-15 22:21:11 +0200

    BugFix: convert tabs to spaces

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -75,13 +75,13 @@ UKF::UKF() {
 
   // Weights of sigma points
   weights_ = VectorXd(n_sigma_);
-	for (int i = 0; i < n_sigma_; ++i) {
-		if (i == 0) {
-			weights_[i] = lambda_ / (lambda_ + n_aug_);
-		} else {
-			weights_[i] = 0.5 / (lambda_ + n_aug_);
-		}
-	}
+  for (int i = 0; i < n_sigma_; ++i) {
+    if (i == 0) {
+      weights_[i] = lambda_ / (lambda_ + n_aug_);
+    } else {
+      weights_[i] = 0.5 / (lambda_ + n_aug_);
+    }
+  }
 
   // predicted sigma points matrix
   Xsig_pred_ = MatrixXd(n_x_, n_sigma_);
@@ -189,68 +189,68 @@ void UKF::Prediction(double dt) {
   vector, x_. Predict sigma points, the state, and the state covariance matrix.
   */
 
-	//create augmented mean vector
-	VectorXd x_aug = VectorXd(n_aug_);
+  //create augmented mean vector
+  VectorXd x_aug = VectorXd(n_aug_);
 
-	//create augmented state covariance
-	MatrixXd P_aug = MatrixXd(n_aug_, n_aug_);
+  //create augmented state covariance
+  MatrixXd P_aug = MatrixXd(n_aug_, n_aug_);
 
-	//create sigma point matrix
-	MatrixXd Xsig_aug = MatrixXd(n_aug_, n_sigma_);
+  //create sigma point matrix
+  MatrixXd Xsig_aug = MatrixXd(n_aug_, n_sigma_);
 
-	//create augmented mean state
-	x_aug.setZero();
-	x_aug.head(n_x_) = x_;
+  //create augmented mean state
+  x_aug.setZero();
+  x_aug.head(n_x_) = x_;
 
-	//create augmented covariance matrix
-	P_aug.setZero();
-	P_aug.topLeftCorner(n_x_, n_x_) = P_;
-	P_aug(n_x_ + 0, n_x_ + 0) = std_a_ * std_a_;
-	P_aug(n_x_ + 1, n_x_ + 1) = std_yawdd_ * std_yawdd_;
+  //create augmented covariance matrix
+  P_aug.setZero();
+  P_aug.topLeftCorner(n_x_, n_x_) = P_;
+  P_aug(n_x_ + 0, n_x_ + 0) = std_a_ * std_a_;
+  P_aug(n_x_ + 1, n_x_ + 1) = std_yawdd_ * std_yawdd_;
 
-	//create square root matrix
-	MatrixXd P_aug_sqrt = P_aug.llt().matrixL();
+  //create square root matrix
+  MatrixXd P_aug_sqrt = P_aug.llt().matrixL();
 
-	//create augmented sigma points
+  //create augmented sigma points
 
-	// first point
-	Xsig_aug.col(0) = x_aug;
+  // first point
+  Xsig_aug.col(0) = x_aug;
 
-	// other points
-	double factor = lambda_ + n_aug_;
-	MatrixXd sigma_points = sqrt(factor) * P_aug_sqrt;
+  // other points
+  double factor = lambda_ + n_aug_;
+  MatrixXd sigma_points = sqrt(factor) * P_aug_sqrt;
 
-	for (int i = 0; i < n_aug_; ++i) {
-		Xsig_aug.col(i + 1) = x_aug + sigma_points.col(i);
-		Xsig_aug.col(i + 1 + n_aug_) = x_aug - sigma_points.col(i);
-	}
+  for (int i = 0; i < n_aug_; ++i) {
+    Xsig_aug.col(i + 1) = x_aug + sigma_points.col(i);
+    Xsig_aug.col(i + 1 + n_aug_) = x_aug - sigma_points.col(i);
+  }
 
-	//predict sigma points
-	for (int i = 0; i < n_sigma_; ++i) {
-		Xsig_pred_.col(i) = f_process_model_function(Xsig_aug.col(i).head(n_x_), Xsig_aug.col(i).bottomRows(n_aug_ - n_x_), dt);
-	}
+  //predict sigma points
+  for (int i = 0; i < n_sigma_; ++i) {
+    Xsig_pred_.col(i) = f_process_model_function(Xsig_aug.col(i).head(n_x_), Xsig_aug.col(i).bottomRows(n_aug_ - n_x_), dt);
+  }
 
-	//predict state mean
-	x_.setZero();
-	for (int i = 0; i < n_sigma_; ++i) {
-		x_ += weights_[i] * Xsig_pred_.col(i);
-	}
+  //predict state mean
+  x_.setZero();
+  for (int i = 0; i < n_sigma_; ++i) {
+    x_ += weights_[i] * Xsig_pred_.col(i);
+  }
 
-	//predict state covariance matrix
-	P_.setZero();
-	for (int i = 0; i < n_sigma_; ++i) {
-		VectorXd Xi_minus_x = Xsig_pred_.col(i) - x_;
+  //predict state covariance matrix
+  P_.setZero();
+  for (int i = 0; i < n_sigma_; ++i) {
+    VectorXd Xi_minus_x = Xsig_pred_.col(i) - x_;
 
-		//angle normalization because of difference calculation above
-		while (Xi_minus_x(3) > M_PI) {
-			Xi_minus_x(3) -= 2. * M_PI;
-		}
-		while (Xi_minus_x(3) < -M_PI) {
-			Xi_minus_x(3) += 2. * M_PI;
-		}
+    //angle normalization because of difference calculation above
+    while (Xi_minus_x(3) > M_PI) {
+      Xi_minus_x(3) -= 2. * M_PI;
+    }
+    while (Xi_minus_x(3) < -M_PI) {
+      Xi_minus_x(3) += 2. * M_PI;
+    }
 
-		P_ += weights_[i] * (Xi_minus_x * Xi_minus_x.transpose());
-	}
+    P_ += weights_[i] * (Xi_minus_x * Xi_minus_x.transpose());
+  }
 }
 
 /**
@@ -267,99 +267,99 @@ void UKF::UpdateLidar(MeasurementPackage measurement_pack) {
   You'll also need to calculate the lidar NIS.
   */
 
-	//set measurement dimension, lidar can measure p_x and p_y
-	int n_z = 2;
+  //set measurement dimension, lidar can measure p_x and p_y
+  int n_z = 2;
 
-	//create matrix for sigma points in measurement space
-	MatrixXd Zsig = MatrixXd(n_z, n_sigma_);
+  //create matrix for sigma points in measurement space
+  MatrixXd Zsig = MatrixXd(n_z, n_sigma_);
 
-	//mean predicted measurement
-	VectorXd z_pred = VectorXd(n_z);
+  //mean predicted measurement
+  VectorXd z_pred = VectorXd(n_z);
 
-	//measurement covariance matrix S
-	MatrixXd S = MatrixXd(n_z, n_z);
+  //measurement covariance matrix S
+  MatrixXd S = MatrixXd(n_z, n_z);
 
-	//transform sigma points into measurement space
-	for (int i = 0; i < n_sigma_; ++i) {
-		Zsig.col(i) = h_lidar_function(Xsig_pred_.col(i)); // + 0: mean of measurement noise
-	}
+  //transform sigma points into measurement space
+  for (int i = 0; i < n_sigma_; ++i) {
+    Zsig.col(i) = h_lidar_function(Xsig_pred_.col(i)); // + 0: mean of measurement noise
+  }
 
-	//calculate mean predicted measurement
-	z_pred.setZero();
-	for (int i = 0; i < n_sigma_; ++i) {
-		z_pred += weights_[i] * Zsig.col(i);
-	}
+  //calculate mean predicted measurement
+  z_pred.setZero();
+  for (int i = 0; i < n_sigma_; ++i) {
+    z_pred += weights_[i] * Zsig.col(i);
+  }
 
-	//calculate innovation covariance matrix S
-	S.setZero();
-	for (int i = 0; i < n_sigma_; ++i) {
-		VectorXd Zi_minus_z_pred = Zsig.col(i) - z_pred;
+  //calculate innovation covariance matrix S
+  S.setZero();
+  for (int i = 0; i < n_sigma_; ++i) {
+    VectorXd Zi_minus_z_pred = Zsig.col(i) - z_pred;
 
-		//angle normalization because of difference calculation above
-		while (Zi_minus_z_pred(1) > M_PI) {
-			Zi_minus_z_pred(1) -= 2. * M_PI;
-		}
-		while (Zi_minus_z_pred(1) < -M_PI) {
-			Zi_minus_z_pred(1) += 2. * M_PI;
-		}
+    //angle normalization because of difference calculation above
+    while (Zi_minus_z_pred(1) > M_PI) {
+      Zi_minus_z_pred(1) -= 2. * M_PI;
+    }
+    while (Zi_minus_z_pred(1) < -M_PI) {
+      Zi_minus_z_pred(1) += 2. * M_PI;
+    }
 
-		S += weights_[i] * (Zi_minus_z_pred * Zi_minus_z_pred.transpose());
-	}
-	//add measurement noise covariance matrix
-	MatrixXd R = MatrixXd(n_z, n_z);
-	R.setZero();
-	R(0, 0) = std_lidar_px_ * std_lidar_px_;
-	R(1, 1) = std_lidar_py_ * std_lidar_py_;
-	S += R;
+    S += weights_[i] * (Zi_minus_z_pred * Zi_minus_z_pred.transpose());
+  }
+  //add measurement noise covariance matrix
+  MatrixXd R = MatrixXd(n_z, n_z);
+  R.setZero();
+  R(0, 0) = std_lidar_px_ * std_lidar_px_;
+  R(1, 1) = std_lidar_py_ * std_lidar_py_;
+  S += R;
 
   //sensor measurement
   VectorXd z = measurement_pack.raw_measurements_;
 
-	//create matrix for cross correlation Tc
-	MatrixXd Tc = MatrixXd(n_x_, n_z);
+  //create matrix for cross correlation Tc
+  MatrixXd Tc = MatrixXd(n_x_, n_z);
 
-	//calculate cross correlation matrix
-	Tc.setZero();
-	for (int i = 0; i < n_sigma_; ++i) {
-		// state difference
-		VectorXd Xi_minus_x = Xsig_pred_.col(i) - x_;
-		//angle normalization because of difference calculation above
-		while (Xi_minus_x(3) > M_PI) {
-			Xi_minus_x(3) -= 2. * M_PI;
-		}
-		while (Xi_minus_x(3) < -M_PI) {
-			Xi_minus_x(3) += 2. * M_PI;
-		}
+  //calculate cross correlation matrix
+  Tc.setZero();
+  for (int i = 0; i < n_sigma_; ++i) {
+    // state difference
+    VectorXd Xi_minus_x = Xsig_pred_.col(i) - x_;
+    //angle normalization because of difference calculation above
+    while (Xi_minus_x(3) > M_PI) {
+      Xi_minus_x(3) -= 2. * M_PI;
+    }
+    while (Xi_minus_x(3) < -M_PI) {
+      Xi_minus_x(3) += 2. * M_PI;
+    }
 
-		//residual
-		VectorXd Zi_minus_z_pred = Zsig.col(i) - z_pred;
-		//angle normalization because of difference calculation above
-		while (Zi_minus_z_pred(1) > M_PI) {
-			Zi_minus_z_pred(1) -= 2. * M_PI;
-		}
-		while (Zi_minus_z_pred(1) < -M_PI) {
-			Zi_minus_z_pred(1) += 2. * M_PI;
-		}
+    //residual
+    VectorXd Zi_minus_z_pred = Zsig.col(i) - z_pred;
+    //angle normalization because of difference calculation above
+    while (Zi_minus_z_pred(1) > M_PI) {
+      Zi_minus_z_pred(1) -= 2. * M_PI;
+    }
+    while (Zi_minus_z_pred(1) < -M_PI) {
+      Zi_minus_z_pred(1) += 2. * M_PI;
+    }
 
-		Tc += weights_[i] * (Xi_minus_x * Zi_minus_z_pred.transpose());
-	}
+    Tc += weights_[i] * (Xi_minus_x * Zi_minus_z_pred.transpose());
+  }
 
-	//calculate Kalman gain K
-	MatrixXd K = Tc * S.inverse();
+  //calculate Kalman gain K
+  MatrixXd K = Tc * S.inverse();
 
-	//residual
-	VectorXd z_minus_z_pred = z - z_pred;
-	//angle normalization because of difference calculation above
-	while (z_minus_z_pred(1) > M_PI) {
-		z_minus_z_pred(1) -= 2. * M_PI;
-	}
-	while (z_minus_z_pred(1) < -M_PI) {
-		z_minus_z_pred(1) += 2. * M_PI;
-	}
+  //residual
+  VectorXd z_minus_z_pred = z - z_pred;
+  //angle normalization because of difference calculation above
+  while (z_minus_z_pred(1) > M_PI) {
+    z_minus_z_pred(1) -= 2. * M_PI;
+  }
+  while (z_minus_z_pred(1) < -M_PI) {
+    z_minus_z_pred(1) += 2. * M_PI;
+  }
 
-	//update state mean and covariance matrix
-	x_ += K * z_minus_z_pred;
-	P_ -= K * S * K.transpose();
+  //update state mean and covariance matrix
+  x_ += K * z_minus_z_pred;
+  P_ -= K * S * K.transpose();
 
   // ToDo: calculate the lidar NIS
 }
@@ -378,110 +378,110 @@ void UKF::UpdateRadar(MeasurementPackage measurement_pack) {
   You'll also need to calculate the radar NIS.
   */
 
-	//set measurement dimension, radar can measure rho, phi, and rho_dot
-	int n_z = 3;
+  //set measurement dimension, radar can measure rho, phi, and rho_dot
+  int n_z = 3;
 
-	//create matrix for sigma points in measurement space
-	MatrixXd Zsig = MatrixXd(n_z, n_sigma_);
+  //create matrix for sigma points in measurement space
+  MatrixXd Zsig = MatrixXd(n_z, n_sigma_);
 
-	//mean predicted measurement
-	VectorXd z_pred = VectorXd(n_z);
+  //mean predicted measurement
+  VectorXd z_pred = VectorXd(n_z);
 
-	//measurement covariance matrix S
-	MatrixXd S = MatrixXd(n_z, n_z);
+  //measurement covariance matrix S
+  MatrixXd S = MatrixXd(n_z, n_z);
 
-	//transform sigma points into measurement space
-	for (int i = 0; i < n_sigma_; ++i) {
-		Zsig.col(i) = h_radar_function(Xsig_pred_.col(i)); // + 0: mean of measurement noise
-	}
+  //transform sigma points into measurement space
+  for (int i = 0; i < n_sigma_; ++i) {
+    Zsig.col(i) = h_radar_function(Xsig_pred_.col(i)); // + 0: mean of measurement noise
+  }
 
-	//calculate mean predicted measurement
-	z_pred.setZero();
-	for (int i = 0; i < n_sigma_; ++i) {
-		z_pred += weights_[i] * Zsig.col(i);
-	}
+  //calculate mean predicted measurement
+  z_pred.setZero();
+  for (int i = 0; i < n_sigma_; ++i) {
+    z_pred += weights_[i] * Zsig.col(i);
+  }
 
-	//calculate innovation covariance matrix S
-	S.setZero();
-	for (int i = 0; i < n_sigma_; ++i) {
-		VectorXd Zi_minus_z_pred = Zsig.col(i) - z_pred;
+  //calculate innovation covariance matrix S
+  S.setZero();
+  for (int i = 0; i < n_sigma_; ++i) {
+    VectorXd Zi_minus_z_pred = Zsig.col(i) - z_pred;
 
-		//angle normalization because of difference calculation above
-		while (Zi_minus_z_pred(1) > M_PI) {
-			Zi_minus_z_pred(1) -= 2. * M_PI;
-		}
-		while (Zi_minus_z_pred(1) < -M_PI) {
-			Zi_minus_z_pred(1) += 2. * M_PI;
-		}
+    //angle normalization because of difference calculation above
+    while (Zi_minus_z_pred(1) > M_PI) {
+      Zi_minus_z_pred(1) -= 2. * M_PI;
+    }
+    while (Zi_minus_z_pred(1) < -M_PI) {
+      Zi_minus_z_pred(1) += 2. * M_PI;
+    }
 
-		S += weights_[i] * (Zi_minus_z_pred * Zi_minus_z_pred.transpose());
-	}
-	//add measurement noise covariance matrix
-	MatrixXd R = MatrixXd(n_z, n_z);
-	R.setZero();
-	R(0, 0) = std_radar_rho_ * std_radar_rho_;
-	R(1, 1) = std_radar_phi_ * std_radar_phi_;
-	R(2, 2) = std_radar_rhod_ * std_radar_rhod_;
-	S += R;
+    S += weights_[i] * (Zi_minus_z_pred * Zi_minus_z_pred.transpose());
+  }
+  //add measurement noise covariance matrix
+  MatrixXd R = MatrixXd(n_z, n_z);
+  R.setZero();
+  R(0, 0) = std_radar_rho_ * std_radar_rho_;
+  R(1, 1) = std_radar_phi_ * std_radar_phi_;
+  R(2, 2) = std_radar_rhod_ * std_radar_rhod_;
+  S += R;
 
   //sensor measurement
   VectorXd z = measurement_pack.raw_measurements_;
 
-	//create matrix for cross correlation Tc
-	MatrixXd Tc = MatrixXd(n_x_, n_z);
+  //create matrix for cross correlation Tc
+  MatrixXd Tc = MatrixXd(n_x_, n_z);
 
-	//calculate cross correlation matrix
-	Tc.setZero();
-	for (int i = 0; i < n_sigma_; ++i) {
-		// state difference
-		VectorXd Xi_minus_x = Xsig_pred_.col(i) - x_;
-		//angle normalization because of difference calculation above
-		while (Xi_minus_x(3) > M_PI) {
-			Xi_minus_x(3) -= 2. * M_PI;
-		}
-		while (Xi_minus_x(3) < -M_PI) {
-			Xi_minus_x(3) += 2. * M_PI;
-		}
+  //calculate cross correlation matrix
+  Tc.setZero();
+  for (int i = 0; i < n_sigma_; ++i) {
+    // state difference
+    VectorXd Xi_minus_x = Xsig_pred_.col(i) - x_;
+    //angle normalization because of difference calculation above
+    while (Xi_minus_x(3) > M_PI) {
+      Xi_minus_x(3) -= 2. * M_PI;
+    }
+    while (Xi_minus_x(3) < -M_PI) {
+      Xi_minus_x(3) += 2. * M_PI;
+    }
 
-		//residual
-		VectorXd Zi_minus_z_pred = Zsig.col(i) - z_pred;
-		//angle normalization because of difference calculation above
-		while (Zi_minus_z_pred(1) > M_PI) {
-			Zi_minus_z_pred(1) -= 2. * M_PI;
-		}
-		while (Zi_minus_z_pred(1) < -M_PI) {
-			Zi_minus_z_pred(1) += 2. * M_PI;
-		}
+    //residual
+    VectorXd Zi_minus_z_pred = Zsig.col(i) - z_pred;
+    //angle normalization because of difference calculation above
+    while (Zi_minus_z_pred(1) > M_PI) {
+      Zi_minus_z_pred(1) -= 2. * M_PI;
+    }
+    while (Zi_minus_z_pred(1) < -M_PI) {
+      Zi_minus_z_pred(1) += 2. * M_PI;
+    }
 
-		Tc += weights_[i] * (Xi_minus_x * Zi_minus_z_pred.transpose());
-	}
+    Tc += weights_[i] * (Xi_minus_x * Zi_minus_z_pred.transpose());
+  }
 
-	//calculate Kalman gain K
-	MatrixXd K = Tc * S.inverse();
+  //calculate Kalman gain K
+  MatrixXd K = Tc * S.inverse();
 
-	//residual
-	VectorXd z_minus_z_pred = z - z_pred;
-	//angle normalization because of difference calculation above
-	while (z_minus_z_pred(1) > M_PI) {
-		z_minus_z_pred(1) -= 2. * M_PI;
-	}
-	while (z_minus_z_pred(1) < -M_PI) {
-		z_minus_z_pred(1) += 2. * M_PI;
-	}
+  //residual
+  VectorXd z_minus_z_pred = z - z_pred;
+  //angle normalization because of difference calculation above
+  while (z_minus_z_pred(1) > M_PI) {
+    z_minus_z_pred(1) -= 2. * M_PI;
+  }
+  while (z_minus_z_pred(1) < -M_PI) {
+    z_minus_z_pred(1) += 2. * M_PI;
+  }
 
-	//update state mean and covariance matrix
-	x_ += K * z_minus_z_pred;
-	P_ -= K * S * K.transpose();
+  //update state mean and covariance matrix
+  x_ += K * z_minus_z_pred;
+  P_ -= K * S * K.transpose();
 
   // ToDo: calculate the radar NIS
 }
 
 VectorXd h_radar_function(VectorXd x) {
-	double p_x = x(0);
-	double p_y = x(1);
-	double v = x(2);
-	double yaw = x(3);
-	double yawd = x(4);
+  double p_x = x(0);
+  double p_y = x(1);
+  double v = x(2);
+  double yaw = x(3);
+  double yawd = x(4);
 
   double rho = sqrt(p_x*p_x + p_y*p_y);
   double theta = atan2(p_y, p_x);
@@ -506,11 +506,11 @@ VectorXd h_radar_function_inverse(VectorXd hx) {
 }
 
 VectorXd h_lidar_function(VectorXd x) {
-	double p_x = x(0);
-	double p_y = x(1);
-	double v = x(2);
-	double yaw = x(3);
-	double yawd = x(4);
+  double p_x = x(0);
+  double p_y = x(1);
+  double v = x(2);
+  double yaw = x(3);
+  double yawd = x(4);
 
   VectorXd result(2);
   result << p_x, p_y;
@@ -527,36 +527,36 @@ VectorXd h_lidar_function_inverse(VectorXd hx) {
 }
 
 VectorXd f_process_model_function(VectorXd x, VectorXd nu, double dt) {
-	double p_x = x[0];
-	double p_y = x[1];
-	double v = x[2];
-	double yaw = x[3];
-	double yawd = x[4];
+  double p_x = x[0];
+  double p_y = x[1];
+  double v = x[2];
+  double yaw = x[3];
+  double yawd = x[4];
 
-	double nu_a = nu[0];
-	double nu_yawdd = nu[1];
+  double nu_a = nu[0];
+  double nu_yawdd = nu[1];
 
-	//avoid division by zero
-	if (std::abs(yawd) < 0.0000001) {
-		// yaw rate is zero
-		x[0] += v * std::cos(yaw) * dt;
-		x[1] += v * std::sin(yaw) * dt;
-	} else {
-		// yaw rate is not zero
-		double yaw_delta = yawd * dt;
-		x[0] += v / yawd * (std::sin(yaw + yaw_delta) - std::sin(yaw));
-		x[1] += v / yawd * (-std::cos(yaw + yaw_delta) + std::cos(yaw));
-	}
-	x[2] += 0;
-	x[3] += yawd * dt;
-	x[4] += 0;
+  //avoid division by zero
+  if (std::abs(yawd) < 0.0000001) {
+    // yaw rate is zero
+    x[0] += v * std::cos(yaw) * dt;
+    x[1] += v * std::sin(yaw) * dt;
+  } else {
+    // yaw rate is not zero
+    double yaw_delta = yawd * dt;
+    x[0] += v / yawd * (std::sin(yaw + yaw_delta) - std::sin(yaw));
+    x[1] += v / yawd * (-std::cos(yaw + yaw_delta) + std::cos(yaw));
+  }
+  x[2] += 0;
+  x[3] += yawd * dt;
+  x[4] += 0;
 
-	// add noise
-	double dt_squared_half = 0.5 * dt * dt;
-	x[0] += dt_squared_half * std::cos(yaw) * nu_a;
-	x[1] += dt_squared_half * std::sin(yaw) * nu_a;
-	x[2] += dt * nu_a;
-	x[3] += dt_squared_half * nu_yawdd;
-	x[4] += dt * nu_yawdd;
-	return x;
+  // add noise
+  double dt_squared_half = 0.5 * dt * dt;
+  x[0] += dt_squared_half * std::cos(yaw) * nu_a;
+  x[1] += dt_squared_half * std::sin(yaw) * nu_a;
+  x[2] += dt * nu_a;
+  x[3] += dt_squared_half * nu_yawdd;
+  x[4] += dt * nu_yawdd;
+  return x;
 }
